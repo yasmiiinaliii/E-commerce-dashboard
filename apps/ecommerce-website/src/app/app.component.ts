2426c664ec092fad4029f6cf2f07c7d2bc17639b
yasmiiinaliii <89663914+yasmiiinaliii@users.noreply.github.com>
2022-03-28 19:37:35 +0200

    initial commit

---
+++ apps/ecommerce-website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'develop-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ecommerce-website';
+}
